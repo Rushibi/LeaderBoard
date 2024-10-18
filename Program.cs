badef11ed55d5be1a00942272da42d49a1c680c9
Rushibi <rushibi1996@gmail.com>
2024-10-18 17:06:15 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace LeaderBoard
+{
+    internal class Program
+    {
+        private static readonly
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost:6379");
+        private static IDatabase db;
+        static void Main(string[] args)
+        {
+             db = redis.GetDatabase();
+            CreateLeaderBoard();
+            DisplayLeaderBoard();
+
+            Console.WriteLine("adding new record");
+            db.SortedSetAdd("leaderboard1","player4",80);
+            
+            DisplayLeaderBoard();
+        }
+        private static void CreateLeaderBoard()
+        {
+            db.SortedSetAdd("leaderboard1", "player1", 100);
+            db.SortedSetAdd("leaderboard1", "player2", 200);
+            db.SortedSetAdd("leaderboard1", "player3",150);
+        }
+        private static void DisplayLeaderBoard()
+        {
+            var leaderboard = db.SortedSetRangeByRankWithScores("leaderboard1", 0, -1, Order.Descending);
+            Console.WriteLine("Leaderboard1:");
+            foreach (var entry in leaderboard)
+            {
+                Console.WriteLine($"{entry.Element}:{entry.Score}");
+            }
+        }
+    }
+}
